00a4119749440d8e3cded1613bdb1d6deecfe167
AlfonArm <120581470+AlfonArm@users.noreply.github.com>
2023-06-22 09:34:33 -0300

    Se añadieron funciones

--- src/axios.js
+++ src/axios.js
@@ -12,4 +12,35 @@ const fetchUserData = async (page, request) => {
     }   
 };
 
-export default fetchUserData
+const deleteUserData = async (page, request) => {     
+    try {       
+        console.log (dir + page);
+        const response = await Axios.delete(dir + page, request);       
+        console.log('respuesta de la api', response);
+        return response
+    } catch (error) {       
+        console.error(error);     
+    }   
+};
+
+const modifyUserData = async (page, request) => {     
+    try {       
+        console.log (dir + page);
+        const response = await Axios.put(dir + page, request);       
+        console.log('respuesta de la api', response);
+        return response
+    } catch (error) {       
+        console.error(error);     
+    }   
+};
+
+const createData = async (page, request) => {     
+    try {       
+        console.log (dir + page);
+        const response = await Axios.post(dir + page, request);       
+        console.log('respuesta de la api', response);
+        return response
+    } catch (error) {       
+        console.error(error);     
+    }   
+};
