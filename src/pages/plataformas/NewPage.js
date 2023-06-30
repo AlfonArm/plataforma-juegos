ec4b72f8520565bd0774b193d73393cd93acbca7
Carlos <catacorac@gmail.com>
2023-06-29 23:44:14 -0300

    plataforma

--- src/pages/plataformas/NewPage.js
+++ src/pages/plataformas/NewPage.js
@@ -1,9 +1,53 @@
-const NewPage = () => {
+import createData from '../../axios/createData';
+
+const newPage = () => {
+
+    const Upload = async (pointer) => {
+        const result = await createData('/plataformas', {name: pointer});
+        console.log(result)
+        return result
+    }
+
+    function exito () {
+        alert ('Se creó la plataforma con éxito. Redirigiendo a la página inicial...')
+        window.location.replace('/plataformas');
+    }
+
+    function fracaso () {
+        document.getElementById('return_plataforma').innerHTML= 'Hubo un error. Intente otra vez';
+    }
+
+    async function subir () {
+        try {
+            const pointer = document.getElementById("nombre_plataforma").value;
+            if ((pointer == null)||(typeof pointer !== 'string')||(pointer.length == 0)) {
+                document.getElementById('return_plataforma').innerHTML= 'Debe insertar un valor válido';
+            } else {
+                const result = await Upload(pointer);
+                result.then (exito, fracaso)
+            }
+        } catch (er) {
+            console.log(er)
+        }
+    }
+
     return (
         <div>
-            <p></p>
+            <form className="cuadro form_chiquito">
+                <div className = "top_form">
+                    <p>Completa:</p>
+                </div>
+                <div className = "flex">
+                    <fieldset>
+                        <legend>Nombre</legend>
+                        <input placeholder="Nombre de la plataforma" id = "nombre_plataforma" type='text'/>
+                        <p id = "return_plataforma"></p>
+                    </fieldset>
+                </div>
+                <input value = "Subir" type="button" className="centro" onClick={() => subir()}/>
+            </form>
         </div>
     )
 }
 
-export default NewPage;
+export default newPage
